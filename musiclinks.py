27a6eca6732849fdb56b043144ac68b546f40d1c
Sanket Ganguly <sanketganguly53@gmail.com>
2025-05-31 17:39:09 +0530

    Upload musiclinks.py

---
+++ musiclinks.py
@@ -0,0 +1,12 @@
+# Demo Dictionary to store song name with Links
+
+links = {
+    "we" : "https://www.youtube.com/watch?v=3AtDnEC4zak",
+    "espresso" : "https://www.youtube.com/watch?v=eVli-tstM5E",
+    "summertime" : "https://www.youtube.com/watch?v=TdrL3QxjyVw",
+    "gata" : "https://www.youtube.com/watch?v=-r687V8yqKY",
+    "attention" : "https://www.youtube.com/watch?v=nfs8NYg7yQM",
+    "faded" : "https://www.youtube.com/watch?v=60ItHLz5WEA",
+    "love" : "https://www.youtube.com/watch?v=725WlG1idPc",
+    "stay" : "https://www.youtube.com/watch?v=kTJczUoc26U"
+}
